1eb26d68d025df14148b4d77769c96c8d1adc70f
vipul-pisal <84674288+vipul-pisal@users.noreply.github.com>
2021-05-23 21:43:56 +0530

    Add files via upload

---
+++ Extra/js/script.js
@@ -0,0 +1,36 @@
+
+$(document).ready(function () {
+    
+    $('.js--section-features').waypoint(function (direction) {
+        if (direction == "down") {
+            $('nav').addClass('sticky');
+        } else {
+            $('nav').removeClass('sticky');
+        }
+    }, {
+        offset: '60px;'
+    });
+      
+    
+        /* Animations on scroll */
+    $('.js--wp-1').waypoint(function(direction) {
+        $('.js--wp-1').addClass('animated fadeInLeft');
+    }, {
+        offset: '50%'
+    });
+
+    $(function() {
+          $('a[href*=#]:not([href=#])').click(function() {
+            if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+              var target = $(this.hash);
+              target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+              if (target.length) {
+                $('html,body').animate({
+                  scrollTop: target.offset().top
+                }, 1000);
+                return false;
+              }
+            }
+          });
+    });
+});
